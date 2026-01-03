d6dd96e86e186ed57a0ebafdf771a1fa0a538b94
Aoife Murray <127849214+murraa57@users.noreply.github.com>
2026-01-03 12:02:06 +0000

    Update finalProject.cpp
    Added geometry for trees, toggled position and scale parameters to test best placements

--- finalProject.cpp
+++ finalProject.cpp
@@ -1,4 +1,4 @@
-// make skybox 
+// make skybox [!!!]
 // make snow
 // make animated skeleton
 // make geometric shapes (trees?)
@@ -188,6 +188,269 @@ struct Skybox {
     }
 };
 
+struct Tree {
+	glm::vec3 position;		// Position of the box 
+	glm::vec3 scale;		// Size of the box in each axis
+	
+	GLfloat vertex_buffer_data[72] = {	// Vertex definition for a canonical box
+		// Front face
+		-1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		
+		// Back face 
+		1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f,
+		
+		// Left face
+		-1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+		-1.0f, 1.0f, 1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Right face 
+		1.0f, -1.0f, 1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		1.0f, 1.0f, 1.0f,
+
+		// Top face
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f, -1.0f, 
+		-1.0f, 1.0f, -1.0f, 
+
+		// Bottom face
+		-1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, -1.0f, 
+		1.0f, -1.0f, 1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f, 
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f, 
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f,  
+	};
+
+	GLuint index_buffer_data[36] = {		// 12 triangle faces of a box
+		0, 1, 2, 	
+		0, 2, 3, 
+		
+		4, 5, 6, 
+		4, 6, 7, 
+
+		8, 9, 10, 
+		8, 10, 11, 
+
+		12, 13, 14, 
+		12, 14, 15, 
+
+		16, 17, 18, 
+		16, 18, 19, 
+
+		20, 21, 22, 
+		20, 22, 23, 
+	};
+
+    // Define UV buffer data
+    GLfloat uv_buffer_data[48] = {
+		// Front
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+
+		// Back
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+
+		// Left
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+
+		// Right
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+
+		// Top - we do not want texture the top
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+
+		// Bottom - we do not want texture the bottom
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 0.0f,
+	};
+    
+	// OpenGL buffers
+	GLuint vertexArrayID; 
+	GLuint vertexBufferID; 
+	GLuint indexBufferID; 
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	GLuint programID;
+ 
+
+	void initialize(glm::vec3 position, glm::vec3 scale) {
+		// Define scale of the tree geometry
+		this->position = position;
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data		
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data 
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Scale UV coordinates such that texture will repeat
+		for (int i = 0; i < 24; ++i) uv_buffer_data[2*i+1] *= 5;
+
+		// Create a vertex buffer object to store the UV data
+		glGenBuffers(1, &uvBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../finalProject/box.vert", "../finalProject/box.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+
+        // Load a texture 
+        //textureID = LoadTextureTileBox("../lab2/facade4.jpg");
+
+        // Get a handle to texture sampler 
+        //textureSamplerID = glGetUniformLocation(programID, "textureSampler");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+        glBindVertexArray(vertexArrayID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// TODO: Model transform 
+		// -----------------------
+		glm::mat4 modelMatrix = glm::mat4();    
+		// Move the tree to its world position first
+		modelMatrix = glm::translate(modelMatrix, position);
+        // Scale the box along each axis to make it look like a tree
+        modelMatrix = glm::scale(modelMatrix, scale);
+        // -----------------------
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// Enable UV buffer and texture sampler
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		// Set textureSampler to use texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(textureSamplerID, 0);
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+        //glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		//glDeleteBuffers(1, &uvBufferID);
+		//glDeleteTextures(1, &textureID);
+		glDeleteProgram(programID);
+	}
+}; 
+
 int main() {
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -208,6 +471,13 @@ int main() {
     Skybox sky;
     sky.initialize();
 
+    Tree tree1, tree2, tree3, tree4;
+	tree1.initialize(glm::vec3(30, 20, -50), glm::vec3(8, 40, 8));
+    tree2.initialize(glm::vec3(-30, 10, 50), glm::vec3(4, 20, 4));
+    tree3.initialize(glm::vec3(-20, 10, 80), glm::vec3(4, 20, 4));
+    tree4.initialize(glm::vec3(5, 5, 40), glm::vec3(2, 10, 2));
+
+
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(90.0f), 4.0f/3.0f, 0.1f, 1000.0f);
 
     while (!glfwWindowShouldClose(window)) {
@@ -222,11 +492,19 @@ int main() {
         glm::mat4 vp = projectionMatrix * viewMatrix;
 
         sky.render(vp);
-        
+        tree1.render(vp);
+        tree2.render(vp);
+        tree3.render(vp);
+        tree4.render(vp);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    tree1.cleanup();
+    tree2.cleanup();
+    tree3.cleanup();
+    tree4.cleanup();
     sky.cleanup();
     glfwTerminate();
     return 0;

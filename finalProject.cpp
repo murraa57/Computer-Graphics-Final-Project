7ea9bfe46e7b4f4239e1709f729b4e7f830252c4
Aoife Murray <127849214+murraa57@users.noreply.github.com>
2026-01-05 14:44:54 +0000

    Update finalProject.cpp
    Attempt at loading in model of christmas tree

--- finalProject.cpp
+++ finalProject.cpp
@@ -5,19 +5,29 @@
 // mesh wireframes (trees)
 // make a fire (?)
 
+
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+// GLTF model loader
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <tiny_gltf.h>
 
 #include <render/shader.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#define _USE_MATH_DEFINES
 #include <math.h>
+
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 static GLFWwindow *window;
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
@@ -29,6 +39,11 @@ static glm::vec3 up(0, 1, 0);
 static float viewAzimuth = 0.f;
 static float viewPolar = 0.f;
 static float viewDistance = 1.0f;
+
+// Lighting  
+//static glm::vec3 lightIntensity(5e6f, 5e6f, 5e6f);
+//static glm::vec3 lightPosition(-275.0f, 500.0f, 800.0f);
+
 
 static GLuint LoadSkyboxTexture(const char *texture_file_path) {
     int w, h, channels;
@@ -476,6 +491,309 @@ struct Present {
 	}
 }; 
 
+
+
+struct MyBot {
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	//GLuint lightPositionID;
+	//GLuint lightIntensityID;
+	GLuint programID;
+    GLuint baseColorTexID;
+
+	tinygltf::Model model;
+
+	// Each VAO corresponds to each mesh primitive in the GLTF model
+	struct PrimitiveObject {
+		GLuint vao;
+		std::map<int, GLuint> vbos;
+        int materialIndex;
+	};
+	std::vector<PrimitiveObject> primitiveObjects;
+
+    // Store OpenGL textures 
+    std::vector<GLuint> glTextures;
+
+	glm::mat4 getNodeTransform(const tinygltf::Node& node) {
+		glm::mat4 transform(1.0f); 
+
+		if (node.matrix.size() == 16) {
+			transform = glm::make_mat4(node.matrix.data());
+		} else {
+			if (node.translation.size() == 3) {
+				transform = glm::translate(transform, glm::vec3(node.translation[0], node.translation[1], node.translation[2]));
+			}
+			if (node.rotation.size() == 4) {
+				glm::quat q(node.rotation[3], node.rotation[0], node.rotation[1], node.rotation[2]);
+				transform *= glm::mat4_cast(q);
+			}
+			if (node.scale.size() == 3) {
+				transform = glm::scale(transform, glm::vec3(node.scale[0], node.scale[1], node.scale[2]));
+			}
+		}
+		return transform;
+	}
+
+	bool loadModel(tinygltf::Model &model, const char *filename) {
+		tinygltf::TinyGLTF loader;
+		std::string err;
+		std::string warn;
+
+		bool res = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
+		if (!warn.empty()) {
+			std::cout << "WARN: " << warn << std::endl;
+		}
+
+		if (!err.empty()) {
+			std::cout << "ERR: " << err << std::endl;
+		}
+
+		if (!res)
+			std::cout << "Failed to load glTF: " << filename << std::endl;
+		else
+			std::cout << "Loaded glTF: " << filename << std::endl;
+
+		return res;
+	}
+
+    void loadTextures() {
+        glTextures.resize(model.images.size());
+        for (size_t i = 0; i < model.images.size(); i++) {
+            const tinygltf::Image& image = model.images[i];
+
+            GLuint tex;
+            glGenTextures(1, &tex);
+            glBindTexture(GL_TEXTURE_2D, tex);
+
+            GLenum format = GL_RGBA;
+            if (image.component == 3) format = GL_RGB;
+
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                format,
+                image.width,
+                image.height,
+                0,
+                format,
+                GL_UNSIGNED_BYTE,
+                image.image.data()
+            );
+
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+            glTextures[i] = tex;
+        }
+    }
+
+	void initialize() {
+		// Modify your path if needed
+		if (!loadModel(model, "../finalProject/scene.gltf")) {
+			return;
+		}
+
+		// Prepare buffers for rendering 
+		primitiveObjects = bindModel(model);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../finalProject/shader/bot.vert", "../finalProject/shader/bot.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for GLSL variables
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+        baseColorTexID = glGetUniformLocation(programID, "baseColorTex");
+		//lightPositionID = glGetUniformLocation(programID, "lightPosition");
+		//lightIntensityID = glGetUniformLocation(programID, "lightIntensity");
+	}
+
+	void bindMesh(std::vector<PrimitiveObject> &primitiveObjects,
+				tinygltf::Model &model, tinygltf::Mesh &mesh) {
+
+		std::map<int, GLuint> vbos;
+		for (size_t i = 0; i < model.bufferViews.size(); ++i) {
+			const tinygltf::BufferView &bufferView = model.bufferViews[i];
+
+			int target = bufferView.target;
+			
+			if (bufferView.target == 0) { 
+				// The bufferView with target == 0 in our model refers to 
+				// the skinning weights, for 25 joints, each 4x4 matrix (16 floats), totaling to 400 floats or 1600 bytes. 
+				// So it is considered safe to skip the warning.
+				//std::cout << "WARN: bufferView.target is zero" << std::endl;
+				continue;
+			}
+
+			const tinygltf::Buffer &buffer = model.buffers[bufferView.buffer];
+			GLuint vbo;
+			glGenBuffers(1, &vbo);
+			glBindBuffer(target, vbo);
+			glBufferData(target, bufferView.byteLength,
+						&buffer.data.at(0) + bufferView.byteOffset, GL_STATIC_DRAW);
+			
+			vbos[i] = vbo;
+		}
+
+		// Each mesh can contain several primitives (or parts), each we need to 
+		// bind to an OpenGL vertex array object
+		for (size_t i = 0; i < mesh.primitives.size(); ++i) {
+
+			tinygltf::Primitive primitive = mesh.primitives[i];
+			tinygltf::Accessor indexAccessor = model.accessors[primitive.indices];
+
+			GLuint vao;
+			glGenVertexArrays(1, &vao);
+			glBindVertexArray(vao);
+
+			for (auto &attrib : primitive.attributes) {
+				tinygltf::Accessor accessor = model.accessors[attrib.second];
+				int byteStride =
+					accessor.ByteStride(model.bufferViews[accessor.bufferView]);
+				glBindBuffer(GL_ARRAY_BUFFER, vbos[accessor.bufferView]);
+
+				int size = 1;
+				if (accessor.type != TINYGLTF_TYPE_SCALAR) {
+					size = accessor.type;
+				}
+
+				int vaa = -1;
+				if (attrib.first.compare("POSITION") == 0) vaa = 0;
+				if (attrib.first.compare("NORMAL") == 0) vaa = 1;
+				if (attrib.first.compare("TEXCOORD_0") == 0) vaa = 2;
+				if (attrib.first.compare("JOINTS_0") == 0) vaa = 3;
+				if (attrib.first.compare("WEIGHTS_0") == 0) vaa = 4;
+				if (vaa > -1) {
+					glEnableVertexAttribArray(vaa);
+					glVertexAttribPointer(vaa, size, accessor.componentType,
+										accessor.normalized ? GL_TRUE : GL_FALSE,
+										byteStride, BUFFER_OFFSET(accessor.byteOffset));
+				} else {
+					std::cout << "vaa missing: " << attrib.first << std::endl;
+				}
+			}
+
+			// Record VAO for later use
+			PrimitiveObject primitiveObject;
+			primitiveObject.vao = vao;
+			primitiveObject.vbos = vbos;
+            primitiveObject.materialIndex = primitive.material; 
+			primitiveObjects.push_back(primitiveObject);
+
+			glBindVertexArray(0);
+		}
+	}
+
+	void bindModelNodes(std::vector<PrimitiveObject> &primitiveObjects, 
+						tinygltf::Model &model,
+						tinygltf::Node &node) {
+		// Bind buffers for the current mesh at the node
+		if ((node.mesh >= 0) && (node.mesh < model.meshes.size())) {
+			bindMesh(primitiveObjects, model, model.meshes[node.mesh]);
+		}
+
+		// Recursive into children nodes
+		for (size_t i = 0; i < node.children.size(); i++) {
+			assert((node.children[i] >= 0) && (node.children[i] < model.nodes.size()));
+			bindModelNodes(primitiveObjects, model, model.nodes[node.children[i]]);
+		}
+	}
+
+	std::vector<PrimitiveObject> bindModel(tinygltf::Model &model) {
+		std::vector<PrimitiveObject> primitiveObjects;
+
+		const tinygltf::Scene &scene = model.scenes[model.defaultScene];
+		for (size_t i = 0; i < scene.nodes.size(); ++i) {
+			assert((scene.nodes[i] >= 0) && (scene.nodes[i] < model.nodes.size()));
+			bindModelNodes(primitiveObjects, model, model.nodes[scene.nodes[i]]);
+		}
+
+		return primitiveObjects;
+	}
+
+	void drawMesh(const std::vector<PrimitiveObject> &primitiveObjects,
+				tinygltf::Model &model, tinygltf::Mesh &mesh) {
+		
+		for (size_t i = 0; i < mesh.primitives.size(); ++i) 
+		{
+			GLuint vao = primitiveObjects[i].vao;
+			std::map<int, GLuint> vbos = primitiveObjects[i].vbos;
+            int materialIndex = primitiveObjects[i].materialIndex;
+
+			glBindVertexArray(vao);
+
+            // Bind the texture
+            if (materialIndex >= 0 && materialIndex < model.materials.size()) {
+                const tinygltf::Material& material = model.materials[materialIndex];
+                if (material.pbrMetallicRoughness.baseColorTexture.index >= 0) {
+                    int textureIndex =
+                        material.pbrMetallicRoughness.baseColorTexture.index;
+                    int imageIndex = model.textures[textureIndex].source;
+
+                    glActiveTexture(GL_TEXTURE0);
+                    glBindTexture(GL_TEXTURE_2D, glTextures[imageIndex]);
+                    glUniform1i(baseColorTexID, 0);
+                }
+            }
+
+			tinygltf::Primitive primitive = mesh.primitives[i];
+			tinygltf::Accessor indexAccessor = model.accessors[primitive.indices];
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbos.at(indexAccessor.bufferView));
+
+			glDrawElements(primitive.mode, indexAccessor.count,
+						indexAccessor.componentType,
+						BUFFER_OFFSET(indexAccessor.byteOffset));
+
+			glBindVertexArray(0);
+		}
+	}
+
+	void drawModelNodes(const std::vector<PrimitiveObject>& primitiveObjects,
+						tinygltf::Model &model, tinygltf::Node &node) {
+		// Draw the mesh at the node, and recursively do so for children nodes
+		if ((node.mesh >= 0) && (node.mesh < model.meshes.size())) {
+			drawMesh(primitiveObjects, model, model.meshes[node.mesh]);
+		}
+		for (size_t i = 0; i < node.children.size(); i++) {
+			drawModelNodes(primitiveObjects, model, model.nodes[node.children[i]]);
+		}
+	}
+	void drawModel(const std::vector<PrimitiveObject>& primitiveObjects,
+				tinygltf::Model &model) {
+		// Draw all nodes
+		const tinygltf::Scene &scene = model.scenes[model.defaultScene];
+		for (size_t i = 0; i < scene.nodes.size(); ++i) {
+			drawModelNodes(primitiveObjects, model, model.nodes[scene.nodes[i]]);
+		}
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+		
+		// Set camera
+		glm::mat4 mvp = cameraMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// Set light data 
+		//glUniform3fv(lightPositionID, 1, &lightPosition[0]);
+		//glUniform3fv(lightIntensityID, 1, &lightIntensity[0]);
+
+		// Draw the GLTF model
+		drawModel(primitiveObjects, model);
+	}
+
+	void cleanup() {
+		glDeleteProgram(programID);
+	}
+}; 
+
 int main() {
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -497,9 +815,13 @@ int main() {
     sky.initialize();
 
     Present present1, present2, present3;
-	present1.initialize(glm::vec3(30, 0, -20), glm::vec3(5, 5, 5));
-    present2.initialize(glm::vec3(12, 0, 2), glm::vec3(1, 1, 1));
-    present3.initialize(glm::vec3(20, 0, 0), glm::vec3(2, 2, 2));
+	present1.initialize(glm::vec3(110, -20, -85), glm::vec3(8, 8, 8));
+    present2.initialize(glm::vec3(68, -20, -55), glm::vec3(2, 2, 2));
+    present3.initialize(glm::vec3(90, -20, -50), glm::vec3(6, 3, 3));
+
+    // Our 3D tree model
+	MyBot bot;
+	bot.initialize();
 
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(90.0f), 4.0f/3.0f, 0.1f, 1000.0f);
 
@@ -519,15 +841,25 @@ int main() {
         present2.render(vp);
         present3.render(vp);
 
+        // Scale the tree model
+        glm::mat4 modelMatrix = glm::mat4(1.0f);
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(1.5f)); 
+        bot.render(vp*modelMatrix);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // Clean up
+	bot.cleanup();
     present1.cleanup();
     present2.cleanup();
     present3.cleanup();
     sky.cleanup();
+    
+    // Close OpenGL window and terminate GLFW
     glfwTerminate();
+
     return 0;
 }
 
